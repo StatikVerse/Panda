a6e796bd083c5108331731c468a26ee86013dddb
StatikVerse <statikverse@gmail.com>
2018-01-27 23:11:24 +0200

    Added Landing Slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShellComponent } from './shell/shell.component';
+import { LandingSliderComponent } from './landing-slider/landing-slider.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShellComponent
+    ShellComponent,
+    LandingSliderComponent
   ],
   imports: [
     BrowserModule
