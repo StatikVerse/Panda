08ed0f714e1df7e54e415b24e5ae190794e58ce8
StatikVerse <statikverse@gmail.com>
2018-01-28 22:58:20 +0200

    added start-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ShellComponent } from './shell/shell.component';
 import { LandingSliderComponent } from './landing-slider/landing-slider.component';
+import { StartButtonComponent } from './start-button/start-button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ShellComponent,
-    LandingSliderComponent
+    LandingSliderComponent,
+    StartButtonComponent
   ],
   imports: [
     BrowserModule
